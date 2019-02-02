b59967a58f13bff0b9e58293cb5f3432542c5df0
loki <tcper@163.com>
2019-02-03 00:43:20 +0800

    first commit

---
+++ build-utils/webpack.common.js
@@ -0,0 +1,54 @@
+const commonPaths = require('./common-paths');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: {
+    vendor: ['semantic-ui-react']
+  },
+  output: {
+    path: commonPaths.outputPath,
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              ["@babel/plugin-proposal-decorators", { "legacy": true }],
+              "react-hot-loader/babel",
+              "@babel/plugin-syntax-dynamic-import",
+              "@babel/plugin-proposal-class-properties"
+            ]
+          }
+        }
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          test: 'vendor',
+          name: 'vendor',
+          enforce: true
+        }
+      }
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `public/index.html`,
+      favicon: `public/favicon.ico`
+    })
+  ]
+};
+
+module.exports = config;
